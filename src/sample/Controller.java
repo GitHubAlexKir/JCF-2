638d666e7a9f23641677151327473d3ac63a1ce4
=alex <alexvanfontys@gmail.com>
2018-02-27 12:37:49 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class Controller {
+    @FXML
+    private TextArea ta_input;
+    @FXML
+    private TextArea ta_output;
+
+    @FXML
+    private void eenFrequentie(){
+        System.out.println("yay!!");
+    }
+
+}
